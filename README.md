f6ee2d6bdc17a15159010aa2061c0ee2c6788382
Drew-bit-dev <d.s.grondahl01@gmail.com>
2022-03-23 13:14:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wtk6le
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wtk6le)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  showOrHide: boolean = true;
+  log: number[] = [];
+  count: number = 0;
+
+  onClick() {
+    this.showOrHide = !this.showOrHide;
+    this.log.push(this.count++);
+  }
+}
